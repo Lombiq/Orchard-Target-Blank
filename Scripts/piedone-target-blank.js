24a5cd38f7c1c94896cee45a01454b75906b784a
Piedone <piedone@pyrocenter.hu>
2013-02-23 20:53:41 +0100

    Putting JS into function and using shorthand document.ready

--- Scripts/piedone-target-blank.js
+++ Scripts/piedone-target-blank.js
@@ -1,15 +1,17 @@
-﻿$(document).ready(function () {
-    $('a:not(.no-target-blank)').each(
-	    function () {
-		    var href = $(this).attr('href');
-				        
-            // Is outgoing link
-		    if (!href.match(/^mailto\:/) && (this.hostname != location.hostname)) {
-                $(this).click(
-                    function () {
-			        window.open(this.href);
-			        return false;
-                });
-		    }
-	    });
-});
+﻿(function ($) {
+    $(function () {
+        $('a:not(.no-target-blank)').each(
+            function () {
+                var href = $(this).attr('href');
+
+                // Is outgoing link
+                if (!href.match(/^mailto\:/) && (this.hostname != location.hostname)) {
+                    $(this).click(
+                        function () {
+                            window.open(this.href);
+                            return false;
+                        });
+                }
+            });
+    });
+})(jQuery);
